58d10534398929936e3d07099542d49e6510465e
mubashirmi <111482469+mubashirmi@users.noreply.github.com>
2025-01-15 13:00:47 +0500

    user to performers

--- src/components/homepage/AllAdmins.jsx
+++ src/components/homepage/AllAdmins.jsx
@@ -648,7 +648,7 @@ function AllAdmins() {
                         htmlFor="userMaxLimit"
                         className="absolute text-gray-600 font-semibold duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#1A73E8] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                       >
-                        Users Max Limit
+                        Performers Max Limit
                       </label>
                     </div>
 
